92caeb4d0125b38efb5d991ec0e7f68b9c1096e1
Zubair-Rajpoot <gokublack008@protonmail.com>
2024-05-13 16:17:02 +0500

    route added for transaction

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -1,6 +1,7 @@
 const productRouter = require("../routes/product.routes");
 const userRouter = require("../routes/user.routes")
 const taskRouter = require("../routes/task.routes")
+const transactionRouter = require("../routes/transaction.routes")
 
 const router = require("express").Router();
 
@@ -9,5 +10,7 @@ router.use("/products", productRouter);
 router.use("/user", userRouter)
 
 router.use("/task", taskRouter)
+
+router.use("/transaction", transactionRouter)
 
 module.exports = router;
